a1d5fa88b39e92efb8490c6a78629e7931074958
Thusharguptha <tg26222@gmail.com>
2026-02-17 04:01:35 +0530

    Solved responsive navbar dropdown

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,4 +7,14 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { }
+export class App {
+  navbarOpen = signal(false);
+
+  toggleNavbar() {
+    this.navbarOpen.set(!this.navbarOpen());
+  }
+
+  closeNavbar() {
+    this.navbarOpen.set(false);
+  }
+}
